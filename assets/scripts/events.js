7558de10370e085c2400e111a97dd169eba45eaf
stgeorge3 <amaxmazzone@gmail.com>
2018-01-06 16:11:03 -0500

    Adds different unit functionality
    Adds ability to choose oz or ml units for both amount used and bottle
    size.
    Adds form data validations
    adds secondary data checks

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,24 +1,36 @@
 const getFormFields = require(`../../lib/get-form-fields`)
 // const ui = require('./ui')
 
-const alcoholCost = function (costOfBottle, sizeOfBottleMl, amtUsedMl) {
-  const costPerMl = costOfBottle / sizeOfBottleMl
-  let amountOwed = costPerMl * amtUsedMl
+const alcoholCostml = function (formData) {
+  let amtUsed = formData.amtUsed
+  let sizeOfBottle = formData.sizeOfBottle
+  if (formData.usedUnit === 'oz') {
+    amtUsed = amtUsed * 29.57
+  }
+  if (formData.bottleUnit === 'oz') {
+    sizeOfBottle = sizeOfBottle * 29.57
+  }
+  const costPerMl = formData.costOfBottle / sizeOfBottle
+  let amountOwed = costPerMl * amtUsed
   amountOwed = +amountOwed.toFixed(2)
   console.log('You owe $' + amountOwed)
   $('#message').text('You owe $' + amountOwed)
   $('#calc').trigger('reset')
 }
 
-const onCalculateSuccess = function (event) {
+const onCalculate = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
   console.log(data)
-  alcoholCost(data.costOfBottle, data.sizeOfBottleMl, data.amtUsedMl)
+  if (data.costOfBottle > 0 && data.sizeOfBottle > 0) {
+    alcoholCostml(data)
+  } else {
+    $('#message').text('incomplete form or incorrect information')
+  }
 }
 
 const addHandlers = function () {
-  $('#calc').on('submit', onCalculateSuccess)
+  $('#calc').on('submit', onCalculate)
 }
 
 module.exports = {
